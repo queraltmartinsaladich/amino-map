bcc6d45f09b062aed0f6b06f7892354f67874bb9
queraltmartinsaladich <queraltmartinsaladich@MacBook-Pro-de-Queralt.local>
2026-03-24 15:19:59 +0100

    “update”

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+if (typeof window !== 'undefined' && window.global === undefined) {
+  window.global = window;
+}
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import 'molstar/build/viewer/molstar.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
