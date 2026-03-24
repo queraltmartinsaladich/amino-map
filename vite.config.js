bf1bf73ab459ec7890591277ed015c6627134a91
queraltmartinsaladich <queraltmartinsaladich@MacBook-Pro-de-Queralt.local>
2026-03-24 15:31:39 +0100

    “update”

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,20 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: './',
-//   define: {
-//     'process.env': {},
-//   },
-//   optimizeDeps: {
-//     include: ['molstar'],
-//   },
-//   css: {
-//     preprocessorOptions: {
-//       scss: {
-//         api: 'modern-compiler' 
-//       },
-//     },
-//   },
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/amino-map/',
+  define: {
+    'process.env': {},
+  },
+  optimizeDeps: {
+    include: ['molstar'],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' 
+      },
+    },
+  },
 })
