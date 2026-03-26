2130cac3b24fb0ed0a985461b0b5475a549696eb
queraltmartinsaladich <queraltmartins@gmail.com>
2026-03-26 09:35:33 +0100

    “update”

--- src/App.jsx
+++ src/App.jsx
@@ -894,7 +894,7 @@ function App() {
                     href="https://molstar.org"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-[10px] font-black text-[#6EB5C0] uppercase tracking-[0.4em] mt-[20px] mb-[20px] hover:underline hover:text-[#FFCCBB]"
+                    className="text-[10px] font-black text-[#6EB5C0] uppercase tracking-[0.4em] hover:underline hover:text-[#FFCCBB]"
                   >
                     molstar.org
                   </a>
@@ -917,7 +917,7 @@ function App() {
                     href="https://uniprot.org"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-[10px] font-black text-[#6EB5C0] uppercase tracking-[0.4em] mb-[20px] hover:underline hover:text-[#FFCCBB]"
+                    className="text-[10px] font-black text-[#6EB5C0] uppercase tracking-[0.4em] hover:underline hover:text-[#FFCCBB]"
                   >
                     uniprot.org
                   </a>
@@ -940,7 +940,7 @@ function App() {
                     href="https://proteinatlas.org"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-[10px] font-black text-[#6EB5C0] uppercase tracking-[0.4em] mb-[20px] hover:underline hover:text-[#FFCCBB]"
+                    className="text-[10px] font-black text-[#6EB5C0] uppercase tracking-[0.4em] hover:underline hover:text-[#FFCCBB]"
                   >
                     proteinatlas.org
                   </a>
@@ -962,7 +962,7 @@ function App() {
                     href="https://geneontology.org"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-[10px] font-black text-[#6EB5C0] uppercase tracking-[0.4em] mb-[20px] hover:underline hover:text-[#FFCCBB]"
+                    className="text-[10px] font-black text-[#6EB5C0] uppercase tracking-[0.4em] hover:underline hover:text-[#FFCCBB]"
                   >
                     geneontology.org
                   </a>
